0e6ebbccf559e80bbd6bdff70b3df6b35b8e8f6f
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2024-12-02 13:25:41 -0600

    Almost done debugging

--- js/main.js
+++ js/main.js
@@ -56,9 +56,11 @@ function toggleCommentButton(postId) {
 }
 
 function deleteChildElements(parentElement) {
+    if (!(parentElement instanceof HTMLElement))
+        return undefined;
     let child = parentElement.lastElementChild;
     while (child) {
-        parentElement.removeChild;
+        parentElement.removeChild(child);
         child = parentElement.lastElementChild;
     }
     return parentElement;
@@ -67,7 +69,7 @@ function deleteChildElements(parentElement) {
 function addButtonListeners() {
     const main = document.querySelector("main");
     const buttonArr = main.querySelectorAll("button");
-    if (buttonArr.length === 0)
+    if (buttonArr === null)
         return null;
     buttonArr.forEach((button) => {
         const postId = button.dataset.postId;
@@ -83,7 +85,7 @@ function addButtonListeners() {
 function removeButtonListeners() {
     const main = document.querySelector("main");
     const buttonArr = main.querySelectorAll("button");
-    if (buttonArr.length === 0)
+    if (buttonArr.length === null)
         return null;
     buttonArr.forEach((button) => {
         const postId = button.dataset.id;
@@ -97,12 +99,14 @@ function removeButtonListeners() {
 }
 
 function createComments(data) {
+    if (!data)
+        return undefined;
     const fragmentElem = document.createDocumentFragment();
     data.forEach((comment) => {
         const articleElem = document.createElement("article");
-        const h3Elem = document.createElemWithText("h3", comment.name);
-        const pBodyElem = document.createElemWithText("p", comment.body);
-        const pEmailElem = document.createElemWithText("p", `From: ${comment.email}`);
+        const h3Elem = createElemWithText("h3", comment.name, "");
+        const pBodyElem = createElemWithText("p", comment.body, "");
+        const pEmailElem = createElemWithText("p", `From: ${comment.email}`, "");
         articleElem.append(h3Elem);
         articleElem.append(pBodyElem);
         articleElem.append(pEmailElem);
@@ -112,6 +116,8 @@ function createComments(data) {
 }
 
 function populateSelectMenu(data) {
+    if (!data)
+        return undefined;
     const selectMenu = document.getElementById("selectMenu")
     const optionArr = createSelectOptions(data);
     optionArr.forEach((option) => {
@@ -132,11 +138,13 @@ async function getUsers() {
 }
 
 async function getUserPosts(userId) {
+    if (!userId)
+        return undefined;
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
         const jsonPostData = await response.json();
         const posts = jsonPostData.filter((post) => {
-            if (post.id === userId)
+            if (post.userId === userId)
                 return post;
         });
         return posts;
@@ -162,6 +170,8 @@ async function getUser(userId) {
 }
 
 async function getPostComments(postId) {
+    if (!postId)
+        return undefined;
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/comments');
         const jsonCommentData = await response.json();
@@ -177,38 +187,42 @@ async function getPostComments(postId) {
 }
 
 async function displayComments(postId) {
+    if (!postId)
+        return undefined;
     const sectionElem = document.createElement("section");
-    sectionElem.dataset.postId;
+    sectionElem.dataset.postId = postId;
     sectionElem.classList.add("comments");
     sectionElem.classList.add("hide");
     const comments = await getPostComments(postId);
     const fragment = createComments(comments);
     sectionElem.append(fragment)
+    return sectionElem;
 }
 
 async function createPosts(data) {
+    if (!data)
+        return undefined;
     const fragmentElem = document.createDocumentFragment();
-    data.forEach(async (post) => {
+    for (const post of data) {
         const articleElem = document.createElement("article");
-        const h2Elem = post.title;
-        const pElemPostBody = post.body;
-        const pElemPostId = `Post ID: ${post.id}`;
+        const h2Elem = document.createElement("h2");
+        h2Elem.textContent = post.title;
+        const pElemPostBody = document.createElement("p");
+        pElemPostBody.textContent = post.body;
+        const pElemPostId = document.createElement("p");
+        pElemPostId.textContent = `Post ID: ${post.id}`;
         const author = await getUser(post.userId);
-        const pElemAuthor = `Author: ${author.name} with ${author.company.name}`;
-        const pElemCompanyPhrase = author.company.catchPhrase;
+        const pElemAuthor = document.createElement("p");
+        pElemAuthor.textContent = `Author: ${author.name} with ${author.company.name}`;
+        const pElemCompanyPhrase = document.createElement("p");
+        pElemCompanyPhrase.textContent = author.company.catchPhrase;
         const button = document.createElement("button");
-        button.textContent("Show Comments");
+        button.textContent = "Show Comments";
         button.dataset.postId = post.id;
-        articleElem.append(h2Elem);
-        articleElem.append(pElemPostBody);
-        articleElem.append(pElemPostId);
-        articleElem.append(pElemAuthor);
-        articleElem.append(pElemCompanyPhrase);
-        articleElem.append(button);
         const section = await displayComments(post.id);
-        articleElem.append(section);
+        articleElem.append(h2Elem, pElemPostBody, pElemPostId, pElemAuthor, pElemCompanyPhrase, button, section);
         fragmentElem.append(articleElem);
-    });
+    }
     return fragmentElem;
 }
 
@@ -220,6 +234,7 @@ async function displayPosts(data) {
     else{
         element = document.createElement("p");
         element.textContent = "Select an Employee to display their posts.";
+        element.classList.add("default-text");
     }
     mainElem.append(element);
     return element;
@@ -227,47 +242,53 @@ async function displayPosts(data) {
 }
 
 function toggleComments(event, postId) {
+    if (!event && !postId)
+        return undefined;
     event.target.listener = true;
     const arr = [];
-    arr.append(toggleCommentSection(postId));
-    arr.append(toggleCommentButton(postId));
+    arr.push(toggleCommentSection(postId));
+    arr.push(toggleCommentButton(postId));
     return arr;
 }
 
 async function refreshPosts(data) {
+    if (!data)
+        return undefined;
     const arr = [];
-    arr.append(removeButtonListeners());
-    arr.append(deleteChildElements(document.querySelector("main")));
-    arr.append(await displayPosts(data));
-    arr.append(addButtonListeners());
+    arr.push(removeButtonListeners());
+    arr.push(deleteChildElements(document.querySelector("main")));
+    arr.push(await displayPosts(data));
+    arr.push(addButtonListeners());
     return arr;
 }
 
 async function selectMenuChangeEventHandler(event) {
-    const arr = [];
-    const selectMenu = document.getElementById("selectMenu");
-    selectMenu.disabled = true;
+    if (!event?.target)
+        return undefined;
     const userId = event.target.value || 1;
-    arr.append(userId);
-    const userPosts = await getUserPosts(userId);
-    arr.append(userPosts);
-    arr.append(await refreshPosts(userPosts));
-    selectMenu.disabled = false;
+    event.target.disabled = true;
+    let arr = [];
+    const postsArr = await getUserPosts(userId);
+    const refreshPostsArr = await refreshPosts(postsArr);
+    arr.push(userId, postsArr, refreshPostsArr);
+    event.target.disabled = false;
     return arr;
 }
 
 async function initPage() {
     const arr = [];
     const users = await getUsers();
-    arr.append(users);
-    arr.append(populateSelectMenu(users));
+    arr.push(users);
+    arr.push(populateSelectMenu(users));
     return arr;
 }
 
 async function initApp() {
     await initPage();
     const selectMenu = document.getElementById("selectMenu");
-    selectMenu.addEventListener("change", selectMenuChangeEventHandler(event));
+    selectMenu.addEventListener("change", selectMenuChangeEventHandler);
 }
 
-document.addEventListener("DOMContentLoaded", initApp());
+document.addEventListener("DOMContentLoaded", initApp);
+
+initApp();
